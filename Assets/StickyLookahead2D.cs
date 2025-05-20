877ac43e4432927163a35241c76b23e91dacf264
Santiago Rosas <santir.mail@gmail.com>
2025-05-20 16:52:52 -0300

    sticky lookahead

---
+++ Assets/StickyLookahead2D.cs
@@ -0,0 +1,25 @@
+// StickyLookahead2D.cs
+using UnityEngine;
+using Unity.Cinemachine;
+
+[RequireComponent(typeof(CinemachineCamera))]
+public class StickyLookahead2D : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private Transform cameraFocus;   // the proxy object
+    [SerializeField] private float aheadDistance = 3f;
+    [SerializeField] private float minSpeed = 0.05f;
+
+    private int _facing = 1;         // 1 = right, -1 = left
+
+    void LateUpdate()
+    {
+        // Update facing only if we're really moving
+        if (Mathf.Abs(playerRb.linearVelocity.x) > minSpeed)
+            _facing = playerRb.linearVelocity.x > 0 ? 1 : -1;
+
+        var p = playerRb.position;
+        cameraFocus.position =
+            new Vector3(p.x + _facing * aheadDistance, p.y, cameraFocus.position.z);
+    }
+}
